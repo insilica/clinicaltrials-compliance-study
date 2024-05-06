35816e2423e5cb6a88626e1829af97a11fdb724e
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-05-06 15:30:50 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "ClinicalTrials Compliance Study";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    dev-shell.url = "github:biobricks-ai/dev-shell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dev-shell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
+          (oldAttrs: {
+            buildInputs = oldAttrs.buildInputs ++ [
+              (python3.withPackages (ps: with ps; [ pandas pyarrow fastparquet ]))
+            ] ++ (with rPackages; [ arrow cthist ]);
+          });
+      });
+}
