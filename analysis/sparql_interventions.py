52e49dfa64bdda54eac70aa9404a7dd0819ab35d
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-22 18:06:23 -0600

    wip on SPARQL results from AACT2024 interventions table

--- analysis/sparql_interventions.py
+++ analysis/sparql_interventions.py
@@ -12,13 +12,35 @@ sparql.setReturnFormat(CSV)
 query_file = Path("analysis/interventions_pubchem.rq")
 query = query_file.read_text()
 
-sparql.setQuery(query)
 
-intervention_names = []
+class Interventions:
+    db_conn: psycopg.Connection = None
+    intervention_names: list = []
 
-with psycopg.connect("dbname=AACT-2024 user=postgres password=postgres") as conn:
-    res = conn.execute("select top 1 * from ctgov.interventions")
-    res.fetchall()
+    def __init__(self):
+        with psycopg.connect(
+            "dbname=AACT-2024 user=postgres password=postgres"
+        ) as conn:
+            self.db_conn = conn
+            res = conn.execute(
+                "select name from ctgov.interventions where intervention_type = 'Drug'"
+            )
+            self.intervention_names.extend(
+                (
+                    name[0]
+                    for name in res.fetchall()
+                    if isinstance(name, tuple) and len(name) == 1
+                )
+            )
+    
+    def __del__(self):
+        self.db_conn.close()
+
+interventions = Interventions()
+
+test_query = query_file.read_text().replace("replaceMe", f"\'{interventions.intervention_names[0]}\'")
+
+sparql.setQuery(test_query)
 
 ret = sparql.queryAndConvert()
 
