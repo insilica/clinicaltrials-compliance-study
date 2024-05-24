2898869418363f18de9acff9c884f333506689bd
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-05-24 08:07:37 -0400

    Add script to download historical records incrementally

---
+++ stages/fetch-cthist-json.pl
@@ -0,0 +1,173 @@
+#!/usr/bin/env perl
+# PODNAME: fetch-cthist-json.pl
+# ABSTRACT: Retrives or updates historical Clinical Trials data
+
+use strict;
+use warnings;
+use feature qw(say signatures postderef);
+no warnings qw(experimental::signatures experimental::postderef);
+
+use Path::Tiny 0.144 qw(path);
+use LWP::UserAgent ();
+use LWP::Protocol::https ();
+use Cpanel::JSON::XS ();
+use List::Util qw(pairs unpairs any);
+use List::UtilsBy qw(nmax_by nsort_by);
+use Types::Standard 2.000 qw(StrMatch Dict Slurpy HashRef ArrayRef);
+use Types::Common::Numeric qw(PositiveOrZeroInt);
+use Type::Params qw(signature_for);
+use Return::Type;
+
+use constant DOWNLOAD_PATH => do {
+	path(
+		( exists $ENV{CTHIST_DOWNLOAD_TEST_DIR_PREFIX}
+		? $ENV{CTHIST_DOWNLOAD_TEST_DIR_PREFIX}
+		: ()
+		),
+		'download/ctgov/historical'
+	);
+};
+
+sub _log { say STDERR @_; }
+
+## Basic types
+use constant {
+	NCT_ID        => StrMatch[qr/ \A NCT [0-9]{8} \z /x ],
+	VersionNumber => PositiveOrZeroInt
+};
+
+## From API
+use constant CTVersionChange         => Dict[version => VersionNumber, Slurpy[HashRef] ];
+use constant {
+	CTStudyRecordData => Dict[studyVersion => VersionNumber, study => HashRef],
+	CTVersionsData    => Dict[changes => ArrayRef[CTVersionChange]],
+};
+
+## Serialization
+use constant StudyRecord => Dict[ change => CTVersionChange, studyRecord => CTStudyRecordData ];
+use constant StudyRecordCollection => ArrayRef[StudyRecord];
+
+my $ua = LWP::UserAgent->new;
+my $json = Cpanel::JSON::XS->new->utf8;
+
+=head2 path_for_nctid
+
+Uses prefix of NCT ID to partition data so that there is not one large
+directory of JSON files.
+
+=cut
+signature_for path_for_nctid => (
+	pos => [ NCT_ID ] );
+sub path_for_nctid($nctid) {
+	return DOWNLOAD_PATH->child(substr($nctid, 0, 6), "${nctid}.json");
+}
+
+sub _fetch_json_or_die($url) {
+	_log("Fetching <$url>");
+	my $res = $ua->get($url);
+
+	if( ! $res->is_success ) {
+		die "Failed to download $url: @{[ $res->message ]}";
+	}
+
+	return $json->decode( $res->decoded_content );
+}
+
+signature_for _get_versions_url => (
+	pos => [ NCT_ID ] );
+sub _get_versions_url($nctid) {
+	return "https://clinicaltrials.gov/api/int/studies/${nctid}/history";
+}
+
+signature_for _get_study_record_url => (
+	pos => [ NCT_ID, VersionNumber ] );
+sub _get_study_record_url($nctid, $version) {
+	return "https://clinicaltrials.gov/api/int/studies/${nctid}/history/${version}";
+}
+
+signature_for get_versions => (
+	pos => [ NCT_ID ] );
+sub get_versions :ReturnType(CTVersionsData) ($nctid) {
+	return _fetch_json_or_die(_get_versions_url($nctid));
+}
+
+signature_for get_study_record => (
+	pos => [ NCT_ID, VersionNumber ]);
+sub get_study_record :ReturnType(CTStudyRecordData) ($nctid, $version) {
+	return _fetch_json_or_die(_get_study_record_url($nctid, $version));
+}
+
+signature_for load_study_records => (
+	pos => [ NCT_ID ]);
+sub load_study_records :ReturnType(StudyRecordCollection) ($nctid) {
+	my $record_file = path_for_nctid($nctid);
+	if( -r $record_file ) {
+		return [
+			map { $json->decode($_) }
+			$record_file->lines_raw( { chomp => 1 } ) ];
+	} else {
+		return [];
+	}
+}
+
+signature_for store_study_records => (
+	pos => [ NCT_ID, StudyRecordCollection ] );
+sub store_study_records($nctid, $records) {
+	_log("$nctid: Writing: " . join(' ', map { $_->{change}{version} } @$records) );
+	my $record_file = path_for_nctid($nctid);
+	$record_file->parent->mkdir;
+	$record_file->spew_raw( [
+		map { $json->encode($_) . "\n" }
+		nsort_by { $_->{change}{version} }
+		@$records
+	]);
+}
+
+sub _has_opt_cutoff_date {
+	return exists $ENV{CTHIST_DOWNLOAD_CUTOFF_DATE}
+		&& $ENV{CTHIST_DOWNLOAD_CUTOFF_DATE} =~ /\A\d{4}-\d{2}-\d{2}\z/a;
+}
+
+signature_for opt_filter_study_records => (
+	pos => [ CTVersionsData ] );
+sub opt_filter_study_records($versions_data) {
+	# CTVersionsData $versions_data
+	# NOTE modifies $versions_data
+	if( _has_opt_cutoff_date ) {
+		$versions_data->{changes}->@* =
+			nmax_by { $_->{version} }
+			grep {
+				# ISO 8601 date → can use string cmp
+				$_->{date} le $ENV{CTHIST_DOWNLOAD_CUTOFF_DATE}
+			}
+			$versions_data->{changes}->@*;
+	}
+}
+
+sub main {
+	my $nctid = shift @ARGV;
+	my $study_records = load_study_records($nctid);
+	my $versions_data =
+		_has_opt_cutoff_date() && @$study_records > 0
+		? { changes => [ map { $_->{change} } @$study_records ] }
+		: get_versions($nctid);
+	_log("$nctid: number of versions: @{[ 0+$versions_data->{changes}->@* ]}");
+	opt_filter_study_records($versions_data);
+	_log("$nctid: number of versions after filtering: @{[ 0+$versions_data->{changes}->@* ]}");
+	for my $change ($versions_data->{changes}->@*) {
+		next if any {
+				$change->{version} == $_->{change}{version}
+			} @$study_records;
+
+		my $study_record = get_study_record($nctid, $change->{version} );
+
+		push @$study_records, {
+			change      => $change,
+			studyRecord => $study_record,
+		};
+
+		store_study_records($nctid, $study_records);
+	}
+}
+
+main;
---
+++ stages/fetch-cthist-json.t
@@ -0,0 +1,163 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use warnings;
+
+use Test::More tests => 2;
+
+use Path::Tiny qw(path);
+use Capture::Tiny qw(capture_stdout capture_stderr tee_stderr);
+use Encode qw(decode_utf8);
+
+my $tmp_dir = Path::Tiny->tempdir;
+$ENV{CTHIST_DOWNLOAD_TEST_DIR_PREFIX} = "$tmp_dir";
+delete $ENV{CTHIST_DOWNLOAD_CUTOFF_DATE};
+
+sub _run_fetch {
+	my ($nctid) = @_;
+	my ($stdout) = capture_stderr {
+		0 == system( './stages/fetch-cthist-json.pl', $nctid ) or die;
+	};
+	diag $stdout =~ s/^/|> /gmr;
+	return $stdout;
+}
+
+sub _run_duckdb {
+	my ($sql) = @_;
+	my $output = capture_stdout {
+		0 == system('duckdb', qw(-noheader -csv -c), $sql) or die;
+	};
+	chomp $output;
+	return $output;
+}
+
+sub _count_study_records {
+	my ($file) = @_;
+
+	my $duckdb_count = 0 + _run_duckdb(<<~SQL);
+		SELECT
+			COUNT(DISTINCT studyRecord.studyVersion)
+				AS answer
+		FROM read_ndjson('$file');
+	SQL
+
+	my $line_count = path($file)->lines_raw;
+
+	die "Mismatch between DuckDB and file!"
+		unless $duckdb_count == $line_count;
+
+	return $duckdb_count;
+}
+
+sub _do_versions_match {
+	my ($file) = @_;
+	return 'true' eq _run_duckdb(<<~SQL)
+		SELECT ALL(
+			SELECT
+				studyRecord.studyVersion == change.version
+			FROM read_ndjson('$file')
+		) AS answer;
+	SQL
+}
+
+subtest "NCT04243421" => sub {
+	my $nctid = 'NCT04243421';
+	my $file = $tmp_dir->child('download/ctgov/historical/NCT042/NCT04243421.json');
+	note "Output will be in file: $file";
+
+	like _run_fetch($nctid),
+		qr/\Qnumber of versions after filtering: 5\E$/m,
+		'expected output';
+
+	ok -r $file, 'has file output';
+
+	is _count_study_records($file), 5, '5 study records';
+
+	ok _do_versions_match($file), 'studyVersion.studyVersion matches change.version';
+
+	like Encode::decode_utf8( _run_duckdb(<<~SQL) ),
+		SELECT
+			DISTINCT
+				studyRecord
+				.study.protocolSection
+				.contactsLocationsModule.locations[1].city
+			AS answer
+		FROM read_ndjson('$file');
+	SQL
+		, qr/\A"Białystok"\z/s,
+		'location contains expected Unicode';
+};
+
+subtest "NCT00000125" => sub {
+	my $nctid = 'NCT00000125';
+	my $file = $tmp_dir->child('download/ctgov/historical/NCT000/NCT00000125.json');
+	note "Output will be in file: $file";
+
+	subtest "NCT00000125 as usual (no cut-off)" => sub {
+		like _run_fetch($nctid),
+			qr/\Qnumber of versions after filtering: 16\E$/m,
+			'expected output';
+
+		ok -r $file, 'has file output';
+
+		is _count_study_records($file), 16, '16 study records';
+
+		ok _do_versions_match($file),
+			'studyVersion.studyVersion matches change.version';
+	};
+
+	subtest "NCT00000125 again but with cut-off" => sub {
+		local $ENV{CTHIST_DOWNLOAD_CUTOFF_DATE} = '2013-09-27';
+
+		ok -r $file, 'has file output already';
+
+		like _run_fetch($nctid),
+			qr/\Qnumber of versions after filtering: 1\E$/m,
+			'expected output';
+
+		ok -r $file, 'has file output';
+
+		is _count_study_records($file), 16,
+			'still 16 study records (existing not removed)';
+
+		note "but now remove the file $file";
+		path($file)->remove;
+
+		_run_fetch($nctid);
+
+		ok -r $file, 'has file output';
+
+		is _count_study_records($file), 1,
+			'only 1 study records fetch when given cut-off';
+	};
+
+	subtest "NCT00000125 again, no cut-off, grab rest of data" => sub {
+		ok -r $file, 'has file output already';
+
+		like _run_fetch($nctid),
+			qr/\Qnumber of versions after filtering: 16\E$/m,
+			'expected output';
+
+		ok -r $file, 'has file output';
+
+		is _count_study_records($file), 16,
+			'now has 16 study records (rest have been added)';
+
+		ok _do_versions_match($file),
+			'studyVersion.studyVersion matches change.version';
+
+		my $change_versions =  _run_duckdb(<<~SQL);
+			SELECT change.version
+			FROM read_ndjson('$file');
+		SQL
+		my @change_versions =  split(/\n/, $change_versions);
+
+		diag explain \@change_versions;
+		is_deeply \@change_versions, [0..15],
+		'change versions are in the correct order';
+	};
+};
+
+
+done_testing;
