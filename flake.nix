c8aba4fb2c0ab24831f3889d098f0e51ec4d1fe1
Ben Lieberman <bhlieberman93@gmail.com>
2024-05-24 14:23:45 -0600

    dependencies updated

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         devShells.default = dev-shell.devShells.${system}.default.overrideAttrs
           (oldAttrs: {
             buildInputs = oldAttrs.buildInputs ++ [
-              (python3.withPackages (ps: with ps; [ pandas pyarrow fastparquet ]))
+              (python3.withPackages (ps: with ps; [ pandas pyarrow fastparquet jsonlines sparqlwrapper ]))
             ] ++ (with rPackages; [ arrow cthist ]);
           });
       });
