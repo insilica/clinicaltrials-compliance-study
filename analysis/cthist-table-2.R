0daf611cc06eb9e185704a17be4179860f7de7a6
bhlieberman <bhlieberman93@gmail.com>
2024-06-25 21:02:36 +0000

    adds rows for trial extensions and updates the summary tables accordingly

--- analysis/cthist-table-2.R
+++ analysis/cthist-table-2.R
@@ -3,23 +3,97 @@
 # issue here:
 # https://github.com/insilica/clinicaltrials-compliance-study/issues/14
 
+library(ggplot2)
+library(gtsummary)
 library(arrow)
 library(dplyr)
+library(tidyverse)
 
 hlacts <- arrow::read_parquet("brick/analysis-20130927/ctgov-studies-hlact.parquet")
 
 # count each of the funding sources table 2 groups the HLACTs according to the
 # three major categories: NIH, Industry, and Other
 count_of_sources <- hlacts |>
-    dplyr::group_by(funding_source) |>
-    dplyr::summarize(n = n())
+  dplyr::group_by(funding_source) |>
+  dplyr::summarize(n = n())
 
 trials_with_results <- hlacts |>
-    dplyr::filter(!is.na(has_results), results_date < "2013-09-27") |>
-    dplyr::group_by(funding_source) |>
-    dplyr::count(has_results, sort = TRUE, name = "count") |>
-    dplyr::ungroup() |>
-    dplyr::mutate(percentage = count/sum(count) * 100) |>
-    dplyr::select(funding_source, count, percentage)
+  filter(!is.na(has_results)) |>
+  dplyr::mutate(results12 = ymd(results_rec_date) <=
+    ymd(primary_completion_date) + months(12)) |>
+  dplyr::filter(funding_source %in% c("NIH", "INDUSTRY", "OTHER")) |>
+  gtsummary::tbl_summary(
+    by = funding_source,
+    label = list(
+      has_results ~
+        "Results reported by September 2013",
+      results12 ~ "Results reported by 12 months after primary completion date",
+      months_to_report_results ~ "Median months to report results"
+    ),
+    statistic = list(
+      all_categorical() ~ "{n} / {N} ({p}%)"
+    ),
+    include = c(
+      "funding_source", "has_results", "results12",
+      "months_to_report_results"
+    ),
+    missing = "no"
+  ) |>
+  add_overall() |>
+  modify_header(label = "**Variable**")
 
 
+trials_w_extension <- hlacts |>
+  filter(!is.na(has_results)) |>
+  mutate(cdisp_date = (coalesce(
+    disp_date, disp_submit_date,
+    disp_qc_date
+  )) < ymd("2013-09-01")) |>
+  filter(!is.na(cdisp_date), funding_source %in% c("INDUSTRY", "NIH", "OTHER")) |>
+  mutate(results_reported = results_date < '2013-09-01') |>
+  tbl_summary(
+    by = funding_source, include = c(
+      "funding_source", "has_results",
+      "cdisp_date", "months_to_report_results", "results_reported"
+    ), missing = "no",
+    label = list(has_results ~ "Trials with extension request by September 2013",
+    months_to_report_results ~ "Median months to report results",
+    results_reported ~ "Results reported before September 2013")
+  ) |>
+  add_overall() |>
+  modify_header(label = "**Variable**")
+
+# this is the segment of Table 2 for
+# all trials that did not submit an extension request
+# like the above datasets, it also groups by
+# median months to report date, and the subset of trials
+# that reported results by September 2013
+trials_no_extension <- hlacts |>
+  filter(!is.na(has_results), ) |>
+  mutate(cdisp_date = (coalesce(
+    disp_date, disp_submit_date,
+    disp_qc_date
+  )) >= ymd("2013-09-01")) |>
+  filter(is.na(cdisp_date), funding_source %in% c("INDUSTRY", "NIH", "OTHER")) |>
+  mutate(results_reported = results_date < '2013-09-01') |>
+  # filter(results_reported) |>
+  tbl_summary(
+    by = funding_source, include = c(
+      "funding_source", "has_results",
+      "cdisp_date", "months_to_report_results", "results_reported"
+    ), missing = "no",
+    label = list(has_results ~ "Trials without extension request by September 2013",
+    months_to_report_results ~ "Median months to report results",
+    results_reported ~ "Results reported before September 2013")
+  ) |>
+  add_overall() |>
+  modify_header(label = "**Variable**")
+
+
+# view the tables
+trials_with_results
+trials_w_extension
+trials_no_extension
+
+# table stacked
+tbl_stack(list(trials_with_results, trials_w_extension, trials_no_extension))
--- sql/create_cthist_hlact.sql
+++ sql/create_cthist_hlact.sql
@@ -1,54 +1,73 @@
 INSTALL postgres;
+
 ATTACH 'postgresql://postgres:postgres@localhost:6432/aact_20240430' as pg (TYPE postgres);
+
 COPY (
-    SELECT *
-    FROM (
+    SELECT
+        *
+    FROM
+        (
             with _all as (
-                SELECT *
-                FROM read_parquet('brick/analysis-20130927/ctgov-studies-all.parquet')
-                WHERE overall_status != 'WITHDRAWN'
+                SELECT
+                    *
+                FROM
+                    read_parquet(
+                        'brick/analysis-20130927/ctgov-studies-all.parquet'
+                    )
+                WHERE
+                    overall_status != 'WITHDRAWN'
                     AND (
-                        primary_completion_date >= '2008-01-01'
+                        strptime(primary_completion_date, '%Y-%m') :: date >= '2008-01-01'
                         OR primary_completion_date IS NULL
                         AND (
-                            completion_date >= '2008-01-01'
+                            strptime(completion_date, '%Y-%m') :: date >= '2008-01-01'
                             OR completion_date IS NULL
                         )
                     )
-                    AND study_type = 'INTERVENTIONAL'
-                    AND phase NOT IN ('EARLY_PHASE1', 'PHASE1')
-                    AND overall_status IN ('TERMINATED', 'COMPLETED')
-                    AND (
-                        primary_completion_date <= '2012-09-01'
-                        OR primary_completion_date IS NULL
+                        AND study_type = 'INTERVENTIONAL'   
+                        AND phase NOT IN ('EARLY_PHASE1', 'PHASE1')
+                        AND overall_status IN ('TERMINATED', 'COMPLETED')
                         AND (
-                            completion_date < '2012-09-01'
-                            OR completion_date IS NULL
+                            strptime(primary_completion_date, '%Y-%m') :: DATE <= '2012-09-01'
+                            OR primary_completion_date IS NULL
+                            AND (
+                                strptime(completion_date, '%Y-%m') :: DATE < '2012-09-01'
+                                OR completion_date IS NULL
+                            )
                         )
-                    )
-                    AND (
-                        primary_completion_date IS NOT NULL
-                        OR completion_date IS NOT NULL
-                        OR (
-                            verification_date >= '2008-01-01'
-                            AND verification_date < '2012-09-01'
+                        AND (
+                            primary_completion_date IS NOT NULL
+                            OR completion_date IS NOT NULL
+                            OR (
+                                strptime(verification_date, '%Y-%m') :: date >= '2008-01-01'
+                                AND strptime(verification_date, '%Y-%m') :: date < '2012-09-01'
+                            )
                         )
+                    ),
+                    aact as (
+                        select
+                            f.nct_id,
+                            any_value(v.has_us_facility) as has_us_facility,
+                            any_value(disposition_first_submitted_date) as extension_date2,
+                            any_value(f.country) as country,
+                            any_value(v.months_to_report_results) as months_to_report_results
+                        from
+                            pg.ctgov.calculated_values v
+                            join pg.ctgov.facilities f on v.nct_id = f.nct_id
+                            join pg.ctgov.studies s on v.nct_id = s.nct_id
+                        where
+                            v.has_us_facility IS NOT NULL
+                        group by
+                            f.nct_id
                     )
-            ),
-            aact as (
-                select f.nct_id,
-                    any_value(v.has_us_facility) as has_us_facility,
-                    any_value(f.country) as country,
-                    any_value(v.months_to_report_results) as months_to_report_results
-                from pg.ctgov.calculated_values v
-                    join pg.ctgov.facilities f on v.nct_id = f.nct_id
-                where v.has_us_facility IS NOT NULL
-                group by f.nct_id
+                select
+                    *
+                from
+                    _all a
+                    join aact ct on ct.nct_id = a.nct_id
+                where
+                    ((is_fda_regulated_drug = true AND primary_purpose = 'INTERVENTIONAL') OR is_fda_regulated_device = true)
+                    AND a.has_us_facility = true
+                    OR ct.has_us_facility = true
             )
-            select *
-            from _all a
-                join aact ct on ct.nct_id = a.nct_id
-            where a.has_us_facility = true
-                OR ct.has_us_facility = true
-        )
-) TO 'brick/analysis-20130927/ctgov-studies-hlact.parquet' (FORMAT PARQUET)
+        ) TO 'brick/analysis-20130927/ctgov-studies-hlact.parquet' (FORMAT PARQUET)
