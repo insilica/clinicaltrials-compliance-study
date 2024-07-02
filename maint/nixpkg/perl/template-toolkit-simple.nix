25a49bd55678f442ffb4694ed55f960ae1f8d697
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-07-02 10:32:31 -0400

    Add Template-Toolkit-Simple

---
+++ maint/nixpkg/perl/template-toolkit-simple.nix
@@ -0,0 +1,16 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlPackage {
+  pname = "Template-Toolkit-Simple";
+  version = "0.31";
+  src = fetchurl {
+    url = "mirror://cpan/authors/id/I/IN/INGY/Template-Toolkit-Simple-0.31.tar.gz";
+    hash = "sha256-WLzGkl2qeNgLQPQ22SGarc4gHc/rnHHpM7zaMMR9WXw=";
+  };
+  propagatedBuildInputs = with perlPackages; [ TemplateToolkit YAMLLibYAML ];
+  meta = {
+    homepage = "https://github.com/ingydotnet/template-toolkit-simple-pm";
+    description = "A Simple Interface to Template Toolkit";
+    license = with lib.licenses; [ artistic1 gpl1Plus ];
+  };
+}
