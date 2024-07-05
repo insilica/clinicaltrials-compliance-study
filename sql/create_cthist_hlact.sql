6caffbdb992cc3909eda0e784ee1703e936925a7
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-07-05 18:14:00 -0400

    Fix whitespace

--- sql/create_cthist_hlact.sql
+++ sql/create_cthist_hlact.sql
@@ -9,7 +9,8 @@ COPY (
         *
     FROM
         (
-            WITH _all AS (
+            WITH
+            _all AS (
                 SELECT
                     *
                 FROM
@@ -26,7 +27,7 @@ COPY (
                             OR completion_date IS NULL
                         )
                     )
-                        AND study_type = 'INTERVENTIONAL'   
+                        AND study_type = 'INTERVENTIONAL'
                         AND phase NOT IN ('EARLY_PHASE1', 'PHASE1')
                         AND overall_status IN ('TERMINATED', 'COMPLETED')
                         AND (
@@ -45,34 +46,34 @@ COPY (
                                 AND strptime(verification_date, '%Y-%m') :: DATE < '2012-09-01'
                             )
                         )
-                    ),
-                    aact AS (
-                        SELECT
-                            f.nct_id,
-                            any_value(v.has_us_facility) as has_us_facility,
-                            any_value(disposition_first_submitted_date) as extension_date2,
-                            any_value(f.country) as country,
-                            any_value(v.months_to_report_results) as months_to_report_results
-                        FROM
-                            pg.ctgov.calculated_values v
-                            JOIN pg.ctgov.facilities f ON v.nct_id = f.nct_id
-                            JOIN pg.ctgov.studies s ON v.nct_id = s.nct_id
-                        WHERE
-                            v.has_us_facility IS NOT NULL
-                        GROUP BY
-                            f.nct_id
-                    )
+            ),
+            aact AS (
                 SELECT
-                    *
+                    f.nct_id,
+                    any_value(v.has_us_facility) as has_us_facility,
+                    any_value(disposition_first_submitted_date) as extension_date2,
+                    any_value(f.country) as country,
+                    any_value(v.months_to_report_results) as months_to_report_results
                 FROM
-                    _all a
-                    JOIN aact ct ON ct.nct_id = a.nct_id
+                    pg.ctgov.calculated_values v
+                    JOIN pg.ctgov.facilities f ON v.nct_id = f.nct_id
+                    JOIN pg.ctgov.studies s ON v.nct_id = s.nct_id
                 WHERE
-                    (
-                        (is_fda_regulated_drug   = true AND primary_purpose = 'INTERVENTIONAL')
-                    OR  (is_fda_regulated_device = true)
-                    )
-                    AND a.has_us_facility = true
-                    OR ct.has_us_facility = true
+                    v.has_us_facility IS NOT NULL
+                GROUP BY
+                    f.nct_id
             )
-        ) TO 'brick/analysis-20130927/ctgov-studies-hlact.parquet' (FORMAT PARQUET)
+            SELECT
+                *
+            FROM
+                _all a
+                JOIN aact ct ON ct.nct_id = a.nct_id
+            WHERE
+                (
+                    (is_fda_regulated_drug   = true AND primary_purpose = 'INTERVENTIONAL')
+                OR  (is_fda_regulated_device = true)
+                )
+                AND a.has_us_facility = true
+                OR ct.has_us_facility = true
+        )
+) TO 'brick/analysis-20130927/ctgov-studies-hlact.parquet' (FORMAT PARQUET)
