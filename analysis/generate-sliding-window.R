92b11b2857dfbca4755c71fe5956b85085c6f730
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-07-31 09:42:23 -0400

    Add sliding window params

---
+++ analysis/generate-sliding-window.R
@@ -0,0 +1,58 @@
+library(lubridate)
+library(glue)
+
+# Generate sliding time periods and format as YAML
+generate_time_periods_yaml <- function(start_date,
+                                       slide_months,
+                                       n_iterations,
+                                       period_length_months = 56) {
+
+  start_date <- ymd(start_date)
+
+  periods <- list()
+
+  for (i in 1:n_iterations) {
+    period_start <- start_date %m+% months((i-1) * slide_months)
+    period_stop <- period_start %m+% months(period_length_months)
+    period_cutoff <- period_stop %m+% months(12)
+
+    period_name.cutoff <- format(period_cutoff, '%Y%m%d')
+
+    period_name <- glue("sliding-window_n-{i}_{period_name.cutoff}")
+    periods[[period_name]] <- list(
+      cutoff = format(period_cutoff, "%Y-%m-%d"),
+      start = format(period_start, "%Y-%m-%d"),
+      stop = format(period_stop, "%Y-%m-%d")
+    )
+  }
+
+  yaml_output <- ""
+
+  for (period_name in names(periods)) {
+    period <- periods[[period_name]]
+    yaml_output <- glue("{yaml_output}
+{period_name}:
+  date:
+    cutoff: '{period$cutoff}'
+    start:  '{period$start}'
+    stop:   '{period$stop}'
+  output:
+    all:            brick/{period_name}/ctgov-studies-all.parquet
+    hlact-filtered: brick/{period_name}/ctgov-studies-hlact.parquet")
+  }
+
+  return(yaml_output)
+}
+
+start_date <- '2008-01-01'
+slide_months <- 56/2
+n_iterations <- 5
+period_length_months <- 56
+
+yaml_output <- generate_time_periods_yaml(start_date,
+                                          slide_months,
+                                          n_iterations,
+                                          period_length_months)
+
+# Print the generated YAML output
+cat(paste0(yaml_output,"\n"))
