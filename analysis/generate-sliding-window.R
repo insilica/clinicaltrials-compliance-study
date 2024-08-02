268475227be5f582ea7e8c7b9d5bfd006c93e524
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-08-01 21:09:05 -0400

    Improve sliding window param generation
    - Add YAML indent.
    - Use subdirectories for each window.

--- analysis/generate-sliding-window.R
+++ analysis/generate-sliding-window.R
@@ -5,24 +5,29 @@ library(glue)
 generate_time_periods_yaml <- function(start_date,
                                        slide_months,
                                        n_iterations,
-                                       period_length_months = 56) {
+                                       period_length_months = 56,
+                                       prefix = 'sliding-window') {
 
   start_date <- ymd(start_date)
 
   periods <- list()
 
   for (i in 1:n_iterations) {
-    period_start <- start_date %m+% months((i-1) * slide_months)
-    period_stop <- period_start %m+% months(period_length_months)
-    period_cutoff <- period_stop %m+% months(12)
+    period_start  <- start_date   %m+% months((i-1) * slide_months)
+    period_stop   <- period_start %m+% months(period_length_months)
+    period_cutoff <- period_stop  %m+% months(12)
 
     period_name.cutoff <- format(period_cutoff, '%Y%m%d')
 
     period_name <- glue("sliding-window_n-{i}_{period_name.cutoff}")
+    period_path <- fs::path_join(c(
+                     glue("{prefix}"),
+                     glue("n-{i}_{period_name.cutoff}")))
     periods[[period_name]] <- list(
       cutoff = format(period_cutoff, "%Y-%m-%d"),
       start = format(period_start, "%Y-%m-%d"),
-      stop = format(period_stop, "%Y-%m-%d")
+      stop = format(period_stop, "%Y-%m-%d"),
+      dir  = period_path
     )
   }
 
@@ -37,11 +42,16 @@ generate_time_periods_yaml <- function(start_date,
     start:  '{period$start}'
     stop:   '{period$stop}'
   output:
-    all:            brick/{period_name}/ctgov-studies-all.parquet
-    hlact-filtered: brick/{period_name}/ctgov-studies-hlact.parquet")
+    all:            brick/{period$dir}/ctgov-studies-all.parquet
+    hlact-filtered: brick/{period$dir}/ctgov-studies-hlact.parquet")
   }
 
-  return(yaml_output)
+  yaml_output <- gsub('(?m)\\A\n','',
+                      yaml_output, perl=TRUE)
+  yaml_indent <- gsub('(?m)^',
+                      paste0(rep(' ', 2),collapse=''),
+                      yaml_output, perl=TRUE)
+  return(yaml_indent)
 }
 
 start_date <- '2008-01-01'
