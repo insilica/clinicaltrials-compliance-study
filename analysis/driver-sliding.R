0712bbc7f5af091565f0230ade1cca5fcfa03c18
Zakariyya Mughal <zaki.mughal@gmail.com>
2024-08-28 09:35:42 -0400

    Compare original data with our window

--- analysis/driver-sliding.R
+++ analysis/driver-sliding.R
@@ -288,7 +288,24 @@ plot.windows.compare.logistic(agg.windows)
 
 plot.windows.stacked.chart(agg.windows)
 
-anderson2015 <- c(
-  window        = anderson2015.window(),
-  hlact.studies = anderson2015.read_and_process()
-)
+plot.compare.anderson2015.agg.window <- function() {
+  anderson2015.original <- list(
+    window        = anderson2015.window(),
+    hlact.studies = anderson2015.read_and_process()
+  )
+  anderson2015.original$window['prefix'] <- 'anderson2015.original'
+  agg.windows.original <- list(
+       anderson2015.original = anderson2015.original
+  )
+  anderson2015.new.windows <- params$param['anderson2015_2008-2012']
+  anderson2015.new.agg.windows <- process.windows.init(anderson2015.new.windows) |>
+    process.windows.amend.results_reported()
+  anderson2015.new.agg.windows[[1]]$window$prefix <- 'anderson2015.new'
+  print(names( anderson2015.new.agg.windows ))
+  names(anderson2015.new.agg.windows) <- c('anderson2015.new')
+  plot.windows.stacked.chart(agg.windows.original, with_names = TRUE)
+  plot.windows.stacked.chart(anderson2015.new.agg.windows, with_names = TRUE)
+}
+
+
+plot.compare.anderson2015.agg.window()
